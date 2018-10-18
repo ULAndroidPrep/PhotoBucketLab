dd87fe4bf777e55d214cb12113185f25c9ddaac9
David Fisher <fisherds@rose-hulman.edu>
2018-10-18 12:04:12 +0100

    image loading

--- PhotoBucket/app/src/main/java/ie/ul/davefisher/photobucket/PhotoDetailActivity.java
+++ PhotoBucket/app/src/main/java/ie/ul/davefisher/photobucket/PhotoDetailActivity.java
@@ -20,6 +20,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.koushikdutta.ion.Ion;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -59,7 +60,7 @@ public class PhotoDetailActivity extends AppCompatActivity {
           mCaptionTextView.setText((String)documentSnapshot.get("caption"));
 
           // https://github.com/koush/ion
-//          Ion.with(mImageView).load((String)documentSnapshot.get("caption"));
+          Ion.with(mImageView).load((String)documentSnapshot.get("imageUrl"));
         }
       }
     });
